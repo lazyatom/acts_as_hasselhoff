bd0d01b214578fc6a81acfcf8e02cc5b0e559730
lazyatom <lazyatom@ca9fbce6-ab17-0410-af20-b66f06baff09>
2006-10-03 01:03:40 +0000

    Set play to be a binary executable
    git-svn-id: http://svn.lazyatom.com/public/plugins/acts_as_hasselhoff@200 ca9fbce6-ab17-0410-af20-b66f06baff09

--- init.rb
+++ init.rb
@@ -1,3 +1,8 @@
 # Include hook code here
 
 ActiveRecord::Base.send(:include, ActsAsHasselhoff)
+
+require 'fileutils'
+puts 'dir: '
+puts directory
+#FileUtils.chmod(0755, File.join(directory, 'play'))
