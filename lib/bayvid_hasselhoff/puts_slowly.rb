e61ffc966959e90fccb1548c787f14a1605c0232
Mario Zigliotto <mariozig@gmail.com>
2014-11-28 13:13:55 -0800

    Add a PutsSlowly class to, you know, puts slowly.

---
+++ lib/bayvid_hasselhoff/puts_slowly.rb
@@ -0,0 +1,20 @@
+module BayvidHasselhoff
+  class PutsSlowly
+    attr_reader :strang
+
+    def initialize(strang)
+      @strang = strang
+    end
+
+    def run(speed = 5.0)
+      thr = Thread.new do
+        @strang.length.times do |i|
+          print "\r#{@strang[0..i]}"
+          sleep(rand / speed)
+          STDOUT.flush
+        end
+      end
+      thr.run
+    end
+  end
+end
