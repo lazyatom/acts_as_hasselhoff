b390213974f024ced7accc6818bcdfcb150ac41d
Mario Zigliotto <mariozig@gmail.com>
2014-11-28 12:27:39 -0800

    Hook into rails' before_save
    Glory

---
+++ lib/bayvid_hasselhoff.rb
@@ -0,0 +1,17 @@
+module BayvidHasselhoff
+  def acts_as_bayvid_hasselhoff
+    include InstanceMethods
+    before_save :play_watch
+  end
+
+  module InstanceMethods
+    def play_watch
+      gem_root = Gem::Specification.find_by_name('bayvid_hasselhoff').gem_dir
+      play_bin = File.join(gem_root, 'bin', 'play')
+      theme_song = File.join(gem_root, 'data', 'sounds', 'theme.m4a')
+      Process.spawn("#{play_bin} #{theme_song}")
+    end
+  end
+end
+
+ActiveRecord::Base.extend BayvidHasselhoff
